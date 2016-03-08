5472e6176434ba040d7c58f0d6de10d37670e31b
Matthew Huber <unixmonky@gmail.com>
2016-03-08 16:54:01 +0000

    reports module rewrite

--- crm/modules/reports/reports.inc.php
+++ crm/modules/reports/reports.inc.php
@@ -78,246 +78,50 @@ function reports_install($old_revision = 0) {
     }
 }
 
+// Initialization //////////////////////////////////////////////////////////////
+
 // Utility functions ///////////////////////////////////////////////////////////
-/**
- * @return A comma-separated list of user emails.
- * @param $opts - Options to pass to member_data().
- */
-function member_email_report ($opts) {
-    $result = array();
-    $data = member_data($opts);
-    foreach ($data as $row) {
-        $email = trim($row['contact']['email']);
-        if (!empty($email)) {
-            $result[] = $email;
-        }
-    }
-    return join($result, ', ');
-}
-
-/**
- * @return the earliest date of any membership.
- */
-function member_membership_earliest_date () {
-    $sql = "SELECT `start` FROM `membership` ORDER BY `start` ASC LIMIT 1";
-    $res = mysql_query($sql);
-    $row = mysql_fetch_assoc($res);
-    if (!$row) {
-        return '';
-    }
-    return $row['start'];
-}
-
-/**
- * @return json structure containing membership statistics.
- */
-function member_statistics () {
-    // Get plans and earliest date
-    $plans = crm_map(member_plan_data(), 'pid');
-    $results = array();
-    foreach ($plans as $pid => $plan) {
-        $results[$pid] = array();
-    }
-    $earliest = member_membership_earliest_date();
-    if (empty($earliest)) {
-        message_register('No membership data available.');
-        return '[]';
-    }
-    // Generate list of months
-    $start = 12*(int)date('Y', strtotime($earliest)) + (int)date('m', strtotime($earliest)) - 1;
-    $now = 12*(int)date('Y') + (int)date('m') - 1;
-    $dates = array();
-    for ($months = $start; $months <= $now; $months++) {
-        $year = floor($months/12);
-        $month = $months % 12 + 1;
-        $dates[] = "('$year-$month-01')";
-    }
-    // Create temporary table with dates
-    $sql = "DROP TEMPORARY TABLE IF EXISTS `temp_months`";
-    $res = mysql_query($sql);
-    if (!$res) { crm_error(mysql_error($res)); }
-    $sql = "CREATE TEMPORARY TABLE `temp_months` (`month` date NOT NULL);";
-    $res = mysql_query($sql);
-    if (!$res) { crm_error(mysql_error($res)); }
-    $sql = "INSERT INTO `temp_months` (`month`) VALUES " . implode(',', $dates) . ";";
-    $res = mysql_query($sql);
-    if (!$res) { crm_error(mysql_error($res)); }
-    // Query number of active memberships for each month
-    $sql = "
-        SELECT
-            `plan`.`pid`
-            , `plan`.`name`
-            , `temp_months`.`month`
-            , UNIX_TIMESTAMP(`temp_months`.`month`) AS `month_timestamp`
-            , count(`membership`.`sid`) AS `member_count`
-        FROM `temp_months`
-        JOIN `plan`
-        LEFT JOIN `membership`
-        ON `membership`.`pid`=`plan`.`pid`
-        AND `membership`.`start` <= `month`
-        AND (`membership`.`end` IS NULL OR `membership`.`end` > `month`)
-        GROUP BY `plan`.`pid`, `month`;
-    ";
-    $res = mysql_query($sql);
-    if (!$res) { crm_error(mysql_error($res)); }
-    // Build results
-    while ($row = mysql_fetch_assoc($res)) {
-        $results[$row['pid']][] = array(
-            'x' => (int)$row['month_timestamp']
-            , 'label' => $row['month']
-            , 'y' => (int)$row['member_count']
-        );
-    }
-    // Convert from associative to indexed
-    $indexed = array();
-    foreach ($results as $pid => $v) {
-        $indexed[] = array(
-            'name' => $plans[$pid]['name'] . " ($pid)"
-            , 'values' => $v
-        );
-    }
-    return json_encode($indexed);
-}
 
 // Forms ///////////////////////////////////////////////////////////////////////
-// Put form generators here
 
+// Tables //////////////////////////////////////////////////////////////////////
+// Put table generators here
+function reports_table ($reportList) {
+    // Determine settings
+    $export = false;
+    // foreach ($opts as $option => $value) {
+    //     switch ($option) {
+    //         case 'export':
+    //             $export = $value;
+    //             break;
+    //     }
+    // }
+
+    // Initialize table
+    $table = array(
+        'columns' => array(
+            array('title' => 'Name')
+            , array('title' => 'Description')
+            , array('title' => 'Action')
+        )
+        , 'rows' => array()
+    );
+
+    foreach ($reportList as $report) {
+        // Add secrets data
+        $row = array();
+        if (user_access('reports_view')) {
+            // Add cells
+            $row[] = $report[1];
+            $row[] = $report[2];
+            $row[] = '<a href=' . crm_url('reports&name=' . $report[0]) . '>Run</a> ';
+        }
+        $table['rows'][] = $row;  
+    }
+    return $table;
+}
 
 // Themeing ////////////////////////////////////////////////////////////////////
-/**
- * @return The themed html for an active member email report.
-*/
-function theme_reports_email ($opts) {
-    $output = '<div class="member-email-report">';
-    $title = '';
-    if (isset($opts['filter']) && isset($opts['filter']['active'])) {
-        $title = $opts['filter']['active'] ? 'Active ' : 'Lapsed ';
-    }
-    $title .= 'Email Report';
-    $output .= "<h2>$title</h2>";
-    $output .= '<textarea rows="10" cols="80">';
-    $output .= member_email_report($opts);
-    $output .= '</textarea>';
-    $output .= '</div>';
-    return $output;
-}
-
-/**
- * @return The themed html for a membership report.
- */
-function theme_reports_membership () {
-    $json = member_statistics();
-    $output = <<<EOF
-<h2>Membership Report</h2>
-<svg id="membership-report" width="960" height="500">
-</svg>
-<script type="text/javascript">
-// Calculate stacking for data
-var layers = $json;
-var stack = d3.layout.stack()
-    .values(function (d) { return d.values; });
-layers = stack(layers);
-var n = layers.length;
-var m = layers[0].values.length;
-
-// Calculate geometry
-var chartWidth = 960,
-    chartHeight = 500;
-var padding = { left:50, bottom:100, top:25, right:50 };
-var width = chartWidth - padding.left - padding.right;
-var height = chartHeight - padding.bottom - padding.top;
-
-// Create scales
-var x = d3.scale.linear()
-    .domain([d3.min(layers, function(layer) { return d3.min(layer.values, function(d) { return d.x; }); }), d3.max(layers, function(layer) { return d3.max(layer.values, function(d) { return d.x; }); })])
-    .range([0, width]);
-var y = d3.scale.linear()
-    .domain([0, d3.max(layers, function(layer) { return d3.max(layer.values, function(d) { return d.y0 + d.y; }); })])
-    .range([height, 0]);
-var color = d3.scale.linear()
-    .range(["#aa3", "#aaf"])
-    .domain([0, 1]);
-
-var colors = [];
-//var roff = Math.round(Math.random()*15);
-//var goff = Math.round(Math.random()*15);
-//var boff = Math.round(Math.random()*15);
-var roff = 12, goff = 7, boff = 8;
-console.log([roff, goff, boff]);
-for (var i = 0; i < layers.length; i++) {
-    var r = ((i+roff) * 3) % 16;
-    var g = ((i+goff) * 5) % 16;
-    var b = ((i+boff) * 7) % 16;
-    colors[i] = '#' + r.toString(16) + g.toString(16) + b.toString(16);
-}
-
-// Set up the svg element
-var svg = d3.select("#membership-report")
-    .attr("width", chartWidth)
-    .attr("height", chartHeight);
-
-// Define axes
-var yaxis = d3.svg.axis().orient('left').scale(y);
-var xlabel = d3.scale.ordinal()
-    .domain(layers[0].values.map(function (d) { return d.label; }))
-    .rangePoints([0, width]);
-var xaxis = d3.svg.axis().orient('bottom').scale(xlabel);
-
-// Draw lines
-var chart = svg.append('g')
-    .attr('transform', 'translate(' + padding.left + ',' + padding.top + ')');
-chart.selectAll('.rule')
-    .data(y.ticks(yaxis.ticks()))
-    .enter()
-    .append('line')
-        .attr('class', 'rule')
-        .attr('x1', '0').attr('x2', width)
-        .attr('y1', '0').attr('y2', '0')
-        .attr('transform', function(d) { return 'translate(0,' + y(d) + ')'; })
-        .style('stroke', '#eee');
-    
-// Draw the data
-var area = d3.svg.area()
-    .x(function(d) { return x(d.x); })
-    .y0(function(d) { return y(d.y0); })
-    .y1(function(d) { return y(d.y0 + d.y); });
-chart.selectAll("path")
-    .data(layers)
-  .enter().append("path")
-    .attr('width', width)
-    .attr("d", function (d) { return area(d.values); })
-    .style("fill", function(d,i) { return colors[i]; });
-
-// Draw the axes
-chart.append('g').attr('id', 'yaxis').attr('class', 'axis').call(yaxis).attr('transform', 'translate(-0.5, 0.5)');
-yaxis.orient('right');
-chart.append('g').attr('id', 'yaxis').attr('class', 'axis').call(yaxis).attr('transform', 'translate(' + (width-0.5) + ',0.5)');
-chart.append('g').attr('id', 'xaxis').attr('class', 'axis').call(xaxis)
-    .attr('transform', 'translate(-0.5,' + (height+0.5) + ')')
-    .selectAll('text')
-        .style('text-anchor', 'end')
-        .attr('dy', '-.35em')
-        .attr('dx', '-9')
-        .attr('transform', 'rotate(-90)');
-d3.selectAll('.axis path').attr('fill', 'none').attr('stroke', 'black');
-
-// Draw a legend
-var legend = chart.append('g').attr('id', 'legend')
-    .selectAll('g').data(layers)
-    .enter()
-    .append('g')
-        .attr('transform', function(d,i) { return 'translate(10,' + ((layers.length - i - 1)*22) + ')'; });
-legend.append('rect')
-    .attr('width', '20').attr('height', '20')
-    .style("fill", function(d,i) { return colors[i]; });
-legend.append('text')
-    .text(function (d) { return d.name; })
-    .attr('transform', 'translate(25, 15)');
-</script>
-EOF;
-    return $output;
-}
-
 
 // Pages ///////////////////////////////////////////////////////////////////////
 
@@ -346,11 +150,26 @@ function reports_page (&$page_data, $page_name, $options) {
         // Set page title
         page_set_title($page_data, 'Reports');
         
-        // Add view, add and import tabs
+        // Include all report_*.inc.php files as those are the individual reports
+        $report_files = glob('modules/reports/report_*.inc.php');
+        $reportList = array();
+        foreach ($report_files as $filename) {
+            require_once("$filename");
+            preg_match('/report_(.*)\.inc.php/', $filename, $match);
+            // reportList = [filename, short name, description]
+            $reportList[] = array(
+                $match[1]
+                , ${'report_'.$match[1].'_name'}
+                , ${'report_'.$match[1].'_desc'}
+                );
+        }
+        // List all reports
         if (user_access('reports_view')) {
-            page_add_content_top($page_data, theme('reports_membership', 'membership'), 'Membership Graph');
-            page_add_content_top($page_data, theme('reports_email', array('filter'=>array('active'=>true))), 'Active Emails');
-            page_add_content_top($page_data, theme('reports_email', array('filter'=>array('active'=>false))), 'Lapsed Emails');
+            page_add_content_top($page_data, theme('table', 'reports', $reportList));
+        }
+        // show report if selected
+        if (isset($options['name'])) {
+            page_add_content_bottom($page_data, theme(${'report_'.$options['name'].'_theme'}, ${'report_'.$options['name'].'_theme_opts'}));
         }
         break;
     }
